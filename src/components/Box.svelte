ae2d7a638b5d1115cee014ece5339906b311655f
fchpr <fchpr>
2020-12-15 21:26:18 -0300

    feat: define box component

---
+++ src/components/Box.svelte
@@ -0,0 +1,31 @@
+<script>
+  let classes = '';
+  export {classes as class};
+</script>
+
+<div class="box {classes}">
+  <slot/>
+</div>
+
+<style>
+  .box {
+    width: 50em;
+    height: 37.5em;
+    margin-top: 3rem;
+    gap: 1em;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-evenly;
+    border: solid 1px #000;
+    background: #fff;
+  }  
+
+  .justify-center {
+    justify-content: center;
+  }
+
+  .sm-height {
+    height: 24em;
+  }
+</style>
