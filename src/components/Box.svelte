48b8bf4cd0bec7097201efe67d0971592a135f2b
fchpr <fchpr>
2020-12-18 21:41:01 -0300

    refactor: add utility classes

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -21,11 +21,39 @@
     background: #fff;
   }  
 
+  .sm-padding-top {
+    padding-top: 2em;
+  }
+
+  .xsm-margin-top {
+    margin-top: 1.5em;
+  }
+
+  .justify-initial {
+    justify-content: initial;
+  }
+
   .justify-center {
     justify-content: center;
   }
 
+  .justify-between {
+    justify-content: space-between;
+  }
+  
+  .l-width {
+    width: 60em;
+  }
+
   .sm-height {
     height: 24em;
+  }
+
+  .xsm-height {
+    height: 5.25em;
+  }
+
+  .row {
+    flex-direction: row;
   }
 </style>
