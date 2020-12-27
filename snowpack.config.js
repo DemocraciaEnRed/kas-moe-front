f9f94a35dacbfd4273ac82c21091369c64bb02c7
fchpr <fchpr>
2020-12-27 13:29:33 -0300

    refactor: support alias for util

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,9 +2,11 @@
 module.exports = {
 	extends: '@sveltejs/snowpack-config',
 	mount: {
-		'src/components': '/_components'
+		'src/components': '/_components',
+		'src/util': '/_util'
 	},
 	alias: {
-		$components: './src/components'
+		$components: './src/components',
+		$util: './src/util'
 	}
 };
