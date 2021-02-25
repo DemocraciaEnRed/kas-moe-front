c320d6820c59e946a45126352dfe7202cd52a634
fchpriani <sietedosfede@gmail.com>
2021-02-25 18:07:29 -0300

    rector: responsible design (wip)

--- src/routes/_stages/preresult.svelte
+++
@@ -1,9 +0,0 @@
-<div class="p-24 h-full flex flex-col gap-24 justify-center items-center bg-white">
-  <div class="w-full flex flex-col gap-8 justify-center items-center">
-    <div class="w-32 h-32 rounded-full bg-moe-gray"/>
-    <h1 class="text-4xl text-moe-blue">Muchas gracias por participar</h1>
-    <h2 class="text-3xl font-light font-sand">Estamos contando los votos</h2>
-    <div class="w-5/12 border-b border-moe-blue"/>
-  </div>
-  <p class="text-xl text-center">Pronto informaremos los resultados de la votación<br> cuando el conteo de votos termine</p>
-</div>
